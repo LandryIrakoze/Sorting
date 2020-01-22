7ac4ab505ed78ad36bfe2f364146a2313438ad47
Landry Irakoze <irakozel@gmail.com>
2020-01-21 19:31:34 -0500

    merge and bubble

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,22 +6,36 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
 
         # TO-DO: swap
-
-
-
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+  def one_pass(arr):
+    for i in range(0, len(arr)-1):
+      left = arr[i]
+      right = arr[i + 1]
+      if right < left:
+        arr[i] = right
+        arr[i + 1] = left
 
-    return arr
+  def check_sorted(arr):
+    is_sorted = True
+    for i in range(0, len(arr) - 1):
+      left = arr[i]
+      right = arr[i + 1]
+      if right < left:
+        is_sorted = False
+    return is_sorted
+
+  while check_sorted(arr) == False:
+    one_pass(arr)
+
+  check_sorted(arr)
+  return arr
 
 
 # STRETCH: implement the Count Sort function below
